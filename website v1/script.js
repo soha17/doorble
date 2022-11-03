d424289b184fc4789f8726d723503b805c3c6125
Jack Gardner <jgardner72@me.com>
2022-11-02 23:54:22 -0400

    completed randomization

--- website v1/script.js
+++ website v1/script.js
@@ -59,6 +59,7 @@ function ddl_loc() {
 }
 function hideAd() {
   // document.getElementById('furniture_ad').style.display = "block";
+  console.log("test")
   const targetDiv = document.getElementById("furniture_ad");
   const btn = document.getElementById("hide_ad");
   btn.onclick = function () {
@@ -68,33 +69,34 @@ function hideAd() {
       targetDiv.style.display = "block";
     }
   };
+
 }
 
 function changeColor(image) {
-  if (image.src.indexOf("images/like_icon.png")>-1) {
-      image.src = "images/like_icon_liked.png"; 
+  if (image.src.indexOf("images/like_icon.png") > -1) {
+    image.src = "images/like_icon_liked.png";
   } else {
-      image.src = "images/like_icon.png";
+    image.src = "images/like_icon.png";
   }
 }
 
 function changeColor_dislike(image) {
-  if (image.src.indexOf("images/dislike_icon.png")>-1) {
-      image.src = "images/dislike_icon_disliked.png"; 
+  if (image.src.indexOf("images/dislike_icon.png") > -1) {
+    image.src = "images/dislike_icon_disliked.png";
   } else {
-      image.src = "images/dislike_icon.png";
+    image.src = "images/dislike_icon.png";
   }
 }
 
 function changeColor_loc(image) {
 
-  if (image.src.indexOf("images/location-icon.png")>-1) {
-      image.src = "images/location-icon-off.png";
-      document.getElementById("loc_status").innerHTML = 'Location Sharing: OFF';
+  if (image.src.indexOf("images/location-icon.png") > -1) {
+    image.src = "images/location-icon-off.png";
+    document.getElementById("loc_status").innerHTML = 'Location Sharing: OFF';
 
   } else {
-      image.src = "images/location-icon.png";
-      document.getElementById("loc_status").innerHTML = 'Location Sharing: ON ';
+    image.src = "images/location-icon.png";
+    document.getElementById("loc_status").innerHTML = 'Location Sharing: ON ';
   }
 }
 
@@ -125,12 +127,12 @@ function randomization(condition) {
       continueInRegistrationButton.classList.add("registerbtn")
       continueInRegistrationButton.textContent = "Continue"
       continueInRegistrationButton.id = "continueInRegistration1"
-      
-      if (document.getElementById("post-body-ads") != null){
+
+      if (document.getElementById("post-body-ads") != null) {
         let postBody = document.getElementById("post-body-ads")
         postBody.appendChild(continueInRegistrationButton)
       }
-      
+
 
       continueInRegistrationButton.onclick = function () {
         location.href = './loc_sft_img.html?rd=1'
@@ -163,13 +165,13 @@ function randomization(condition) {
       continueInRegistrationButtonLft.classList.add("registerbtn")
       continueInRegistrationButtonLft.textContent = "Continue"
       continueInRegistrationButtonLft.id = "continueInRegistration1"
-      
 
-      if (document.getElementById("lft-vid-post-body-ads") != null){
+
+      if (document.getElementById("lft-vid-post-body-ads") != null) {
         let postBody = document.getElementById("lft-vid-post-body-ads")
         postBody.appendChild(continueInRegistrationButtonLft)
       }
-  
+
       continueInRegistrationButtonLft.onclick = function () {
         location.href = './loc_lft_video.html?rd=2'
       }
@@ -178,11 +180,11 @@ function randomization(condition) {
       finishRegistrationButtonLft.type = "submit"
       finishRegistrationButtonLft.classList.add("registerbtn")
       finishRegistrationButtonLft.textContent = "Finish Registration"
-      
+
 
       let postBodyLocLft = document.getElementById("lft-vid-post-body-loc")
       postBodyLocLft.appendChild(finishRegistrationButtonLft)
-      
+
       finishRegistrationButtonLft.onclick = function () {
         completionDiv.textContent = "Your completion code is 28314. You may now return to the survey."
         let postBody = document.getElementById("lft-vid-post-body-loc")
@@ -197,23 +199,45 @@ function randomization(condition) {
         let doorblePost = document.getElementById("doorble-post")
         doorblePost.appendChild(completionDiv)
       }
-      //TODO: add functionality to handle the second task where a participant watches the first ad they see.
+
+      let hideAdElement = document.getElementById("hide_ad")
+      hideAdElement.onclick = function () {
+        hideAd()
+        let abovePost = document.getElementById("doorble-post")
+        completionDiv.textContent = "Your completion code is 78782. You may now return to the survey."
+        abovePost.appendChild(completionDiv)
+      }
       break;
     case 4:
       //help center
+      if (document.getElementById("helpCenter") != null) {
+        let helpCenter = document.getElementById("helpCenter")
+        helpCenter.href = './helpcenter.html?rd=4'//TODO ensure participant id still captured
+      }
+
       if (document.getElementById("hcAdvertisements") != null) {
         let hcAdvertisements = document.getElementById("hcAdvertisements")
         hcAdvertisements.onclick = function () {
-          location.href = './ads_sft_img.html'
+          location.href = './ads_sft_img.html?rd=4'
         }
       }
       let finishTaskButton = document.createElement("button")
       finishTaskButton.type = "submit"
       finishTaskButton.classList.add("registerbtn")
-      finishTaskButton.textContent = "Finish review of this page"
+      finishTaskButton.textContent = "I'm done!"
       finishTaskButton.onclick = function () {
-        location.href = './completion.html'
+        if (document.getElementById("post-body-ads") != null) {
+          let postBodyAdsFinish = document.getElementById("post-body-ads")
+          completionDiv.textContent = "Your completion code is 13408. You may now return to the survey."
+          postBodyAdsFinish.appendChild(completionDiv)
+        }
+        else {
+          let postBodyLocFinish = document.getElementById("post-body-loc")
+          completionDiv.textContent = "Your completion code is 13408. You may now return to the survey."
+          postBodyLocFinish.appendChild(completionDiv)
+        }
       }
+
       if (document.getElementById("post-body-ads") != null) {
         let postBodyAds = document.getElementById("post-body-ads")
         postBodyAds.appendChild(finishTaskButton)
@@ -222,7 +246,7 @@ function randomization(condition) {
       if (document.getElementById("hcLocation") != null) {
         let hcLocation = document.getElementById("hcLocation")
         hcLocation.onclick = function () {
-          location.href = './loc_sft_img.html'
+          location.href = './loc_sft_img.html?rd=4'
         }
       }
 
@@ -234,45 +258,66 @@ function randomization(condition) {
       break;
     case 5:
       //help center
+      if (document.getElementById("helpCenter") != null) {
+        let helpCenter = document.getElementById("helpCenter")
+        helpCenter.href = './helpcenter.html?rd=5'//TODO ensure participant id still captured
+      }
+
       if (document.getElementById("hcAdvertisements") != null) {
         let hcAdvertisements = document.getElementById("hcAdvertisements")
         hcAdvertisements.onclick = function () {
-          location.href = './ads_lft_video.html'
+          location.href = './ads_lft_video.html?rd=5'
         }
       }
       let finishTaskButton5 = document.createElement("button")
       finishTaskButton5.type = "submit"
       finishTaskButton5.classList.add("registerbtn")
-      finishTaskButton5.textContent = "Finish review of this page"
+      finishTaskButton5.textContent = "I'm done!"
       finishTaskButton5.onclick = function () {
-        location.href = './completion.html'
+        if (document.getElementById("lft-vid-post-body-ads") != null) {
+          let postBodyAds = document.getElementById("lft-vid-post-body-ads")
+          completionDiv.textContent = "Your completion code is 37007. You may now return to the survey."
+          postBodyAds.appendChild(completionDiv)
+        }
+        if (document.getElementById("lft-vid-post-body-loc") != null) {
+          let postBodyLoc = document.getElementById("lft-vid-post-body-loc")
+          completionDiv.textContent = "Your completion code is 37007. You may now return to the survey."
+          postBodyLoc.appendChild(completionDiv)
+        }
+
       }
-      if (document.getElementById("post-body-ads") != null) {
-        let postBodyAds = document.getElementById("post-body-ads")
+      if (document.getElementById("lft-vid-post-body-ads") != null) {
+        let postBodyAds = document.getElementById("lft-vid-post-body-ads")
         postBodyAds.appendChild(finishTaskButton5)
       }
 
       if (document.getElementById("hcLocation") != null) {
         let hcLocation = document.getElementById("hcLocation")
         hcLocation.onclick = function () {
-          location.href = './loc_lft_video.html'
+          location.href = './loc_lft_video.html?rd=5'
         }
       }
 
-      if (document.getElementById("post-body-loc") != null) {
-        let postBodyLoc = document.getElementById("post-body-loc")
+      if (document.getElementById("lft-vid-post-body-loc") != null) {
+        let postBodyLoc = document.getElementById("lft-vid-post-body-loc")
         postBodyLoc.appendChild(finishTaskButton5)
       }
       break;
     case 6:
       //settings
+      if (document.getElementById("settings") != null) {
+        let settings = document.getElementById("settings")
+        settings.href = './settings.html?rd=6'
+      }
+
       let settingsAds = document.getElementById("adSlider")
       settingsAds.onclick = function () {
         let div = document.createElement("div")
         let button = document.createElement("button")
         button.textContent = "Finish review of this setting"
         button.onclick = function () {
-          location.href = './completion.html'
+          completionDiv.textContent = "Your completion code is 16717. You may now return to the survey."
+          document.getElementById("ads_education").appendChild(completionDiv)
         }
         div.appendChild(button)
         document.getElementById("ads_education").appendChild(div)
@@ -283,22 +328,57 @@ function randomization(condition) {
         let button = document.createElement("button")
         button.textContent = "Finish review of this setting"
         button.onclick = function () {
-          location.href = './completion.html'
+          completionDiv.textContent = "Your completion code is 16717. You may now return to the survey."
+          document.getElementById("location_education").appendChild(completionDiv)
         }
         div.appendChild(button)
         document.getElementById("location_education").appendChild(div)
       }
       break;
     case 7:
-      //in feed
-      // TODO (similar code will apply for 8 & 9): similar to above, use query string to show location education 
-      //when visting website for second time.
+      //in feed -sft
+      let natureDislike = document.getElementById("natureDislike")
+      natureDislike.onclick = function () {
+        completionDiv.textContent = "Your completion code is 91643. You may now return to the survey."
+        document.getElementById("naturePostActions").appendChild(completionDiv)
+      }
       break;
     case 8:
-      //in feed
+      //in feed -sft-image
+      let inFeedAdsSft = document.getElementById("in-feed-ads-sft")
+      inFeedAdsSft.style.display = "none"
+      let inFeedLocSft = document.getElementById("in-feed-location-sft")
+      inFeedLocSft.style.display = "none"
+
+      let inFeedAdsSftImage = document.getElementById("in-feed-ads-sft-image")
+      inFeedAdsSftImage.style.display = "block"
+      let inFeedLocSftImage = document.getElementById("in-feed-location-sft-image")
+      inFeedLocSftImage.style.display = "block"
+
+      let natureDislike2 = document.getElementById("natureDislike")
+      natureDislike2.onclick = function () {
+        completionDiv.textContent = "Your completion code is 69927. You may now return to the survey."
+        document.getElementById("naturePostActions").appendChild(completionDiv)
+      }
       break;
     case 9:
-      //in feed
+      //in feed -video
+      let inFeedAdsSft2 = document.getElementById("in-feed-ads-sft")
+      inFeedAdsSft2.style.display = "none"
+      let inFeedLocSft2 = document.getElementById("in-feed-location-sft")
+      inFeedLocSft2.style.display = "none"
+
+      let inFeedAdsVideo = document.getElementById("in-feed-ads-video")
+      inFeedAdsVideo.style.display = "block"
+      let inFeedLocVideo = document.getElementById("in-feed-location-video")
+      inFeedLocVideo.style.display = "block"
+
+      let natureDislike3 = document.getElementById("natureDislike")
+      natureDislike3.onclick = function () {
+        completionDiv.textContent = "Your completion code is 68587. You may now return to the survey."
+        document.getElementById("naturePostActions").appendChild(completionDiv)
+      }
+
       break;
   }
 }
